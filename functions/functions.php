a3540f0521b38c186d32486d2bb75ab29756df13
burillu <burillu90@hotmail.it>
2023-11-30 16:49:42 +0100

    Milestone 2 completed

--- functions/functions.php
+++ functions/functions.php
@@ -3,7 +3,22 @@ function print_prova(){
     var_dump("Pippo");;
 }
 
-
+//funzione per generare password
+function generate_password()
+{
+  $symbols = '!?&%$<>^+-*/()[]{}@#_=';
+  $char = 'abcdefghijklmnopqrstuvwxyz';
+  $char_capital = strtoupper($char);
+  $all_char = $symbols . $char . $char_capital;
+  //var_dump($all_char);
+  $new_pasw = '';
+  while (strlen($new_pasw) < $_GET['length']) {
+    # code...
+    $new_pasw .= $all_char[rand(0, strlen($all_char) - 1)];
+  }
+  // var_dump($new_pasw);
+  return $new_pasw;
+}
 
 
 ?>
--- index.php
+++ index.php
@@ -6,22 +6,7 @@ if (isset($_GET["length"])) {
   $length = (int) $_GET["length"];
   //var_dump($length);
 
-  //funzione per generare password
-  function generate_password()
-  {
-    $symbols = '!?&%$<>^+-*/()[]{}@#_=';
-    $char = 'abcdefghijklmnopqrstuvwxyz';
-    $char_capital = strtoupper($char);
-    $all_char = $symbols . $char . $char_capital;
-    //var_dump($all_char);
-    $new_pasw = '';
-    while (strlen($new_pasw) < $_GET['length']) {
-      # code...
-      $new_pasw .= $all_char[rand(0, strlen($all_char) - 1)];
-    }
-    // var_dump($new_pasw);
-    return $new_pasw;
-  }
+  
   $result = generate_password();
 }
 
